cf2c095c199c79404c8084d70a50fb32d5949227
Joseph Lawrence <joseph.lawrence-2@student.manchester.ac.uk>
2021-05-26 11:27:53 +0100

    Refactoring ChessBoard row rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import ChessBoard from "./ChessBoard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1>Chess</h1>
-          <ChessBoard />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Chess</h1>
+                <ChessBoard/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
